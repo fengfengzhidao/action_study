b09f36d034f2c7686f9332917fcc011b9e13c3d3
枫枫 <guozhiyong@range-soft.com>
2025-06-20 15:45:50 +0800

    打印信息

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
+var (
+	version   = "" // 编译时注入
+	buildTime = "" // 编译时注入
+	gitCommit = "" // 编译时注入
+)
+
+func info(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"version":   version,
+		"buildTime": buildTime,
+		"gitCommit": gitCommit,
+	})
+}
+
 func v1(c *gin.Context) {
 	c.JSON(200, gin.H{"code": 0, "version": "v1"})
 }
@@ -20,5 +34,6 @@ func main() {
 	router.GET("hello", hello)
 	router.GET("v1", v1)
 	router.GET("v2", v2)
+	router.GET("", info)
 	router.Run(":8000")
 }
